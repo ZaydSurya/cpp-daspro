835422e0f0612896d7546a4f35d8e250839ce9b6
Zayd Surya Marthana <zaydsuryamarthana@gmail.com>
2025-10-06 11:47:45 +0700

    upd-p4
    update pertemuan 4

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main(){
         cout << "--------------------------------------" << endl;
         cout << "1. Pertemuan 1 : Pengenalan C++" << endl;
         cout << "2. Pertemuan 2 : Tipe Data" << endl;
-        cout << "3. Pertemuan 3 : Variabel dan Logika" << endl;
+        cout << "3. Pertemuan 3 : Variabel dan Logika" << endl; 
         cout << "4. Pertemuan 4 : Analisis If Else\n" << endl;
         cout << "Pilih salah satu pembelajaran C++ : "; cin >> pilihan;
         cout << '\n';
--- p4.cpp
+++ p4.cpp
@@ -11,38 +11,57 @@ void dataP4(){
 }
 
 void kasus1P4(){
-
-    cout << "Hello World" << endl;
-
-}
-
-void kasus2P4(){
     int pilihan;
     auto soal1P4 = [](){
-        float a,hasil;
+        double a;
         cout << "Program Nilai Positif (+) dan Negatif (-)" << endl;
         cout << "-----------------------------------------" << endl;
         cout << "Konversikan nilai integer atau angka ke dalam Positif\n" << endl;
         cout << "Masukkan nilai : "; cin >> a;
         if (a<0){
-            hasil = a * -1;
+            a = a * -1;
             cout << "---JAWABAN---" << endl;
             cout << "Nilai kamu akan dikonversikan ke bilangan positif" << endl;
-            cout << "Hasil akhir : " << hasil << endl;
+            cout << "Hasil akhir : " << a << endl;
             cout << '\n' << endl;
         } else if (a>=0){
             cout << "Nilai kamu adalah : " << a << " Bilangin ini adalah bernilai positif" << endl;
         } else {
             cout << "Input tidak valid, coba ulangi kembali.\n" << endl;
         }
-
-
     };
+
+    auto soal2P4 = [](){
+        double a,b;
+        cout << "Program Nilai Variabel" << endl;
+        cout << "----------------------" << endl;
+        cout << "Masukkan nilai A : "; cin >> a;
+        cout << "Masukkan Nilai B : "; cin >> b;
+
+        if(a>b){
+            if(a>0){
+            cout << "/n------Keterangan------" << endl;
+            cout << "Nilai A atau " << a << " lebih besar dari nilai B" << endl;
+            cout << "Nilai A adalah Bilangan Positif\n" << endl;
+            } else {
+                cout << "Nilai A lebih besar dari B yaitu " << a << " akan tetapi bernilai Negatif" << endl;
+                a = a*-1;
+                cout << "Nilai A sekarang adalah " << a << ". Sekarang nilainya adalah positif\n" << endl;
+            }
+
+        } else {
+            cout << "/n------Keterangan------" << endl;
+            cout << "Nilai B atau " << b << " lebih besar dari nilai A" << endl;
+            cout << "Nilai B adalah Bilangan Positif\n" << endl;
+        }
+    };
+
     cout << "Program Matematika Sederhana :" << endl;
     cout << "------------------------------" << endl;
     cout << "Studi Kasus Kondisi Matematika Sederhana" << endl;
     cout << "1. Nilai Positif atau Negatif" << endl;
     cout << "2. Nilai dari Dua Variabel" << endl;
+    cout << "3. Membalikan Nilai [Belum Ada]" << endl;
     cout << "------------------------------" << endl;
     cout << "0. Kembali\n" << endl;
     cout << "Pilih salah satu program : "; cin >> pilihan;
@@ -51,13 +70,63 @@ void kasus2P4(){
     case 1 :
         soal1P4();
         break;
+    case 2 :
+        soal2P4();
+        break;
     default :
         cout << "Input tidak ditemukan atau tidak valid.\n";
     }
-
-
 }
 
+void kasus2P4(){
+    double mtk,ipa,desain,indo,inggris,daspro;
+    string nama, nim;
+    cout << "Program Nilai Raport" << endl;
+    cin.ignore(numeric_limits<streamsize>::max(),'\n');
+    cout << "Nama Lengkap\t: "; getline(cin,nama);
+    cout << "NIM\t\t: "; getline(cin,nim);
+    cout << "-----------------------------------" << endl;
+    cout << "1. Dasar Pemrograman\t\t: "; cin >> daspro;
+    cout << "2. Matematika\t\t\t: "; cin >> mtk;
+    cout << "3. Ilmu Pengetahuan Alam\t: "; cin >> ipa;
+    cout << "4. Desain Grafis\t\t: "; cin >> desain;
+    cout << "5. Bahasa Indonesia\t\t: "; cin >> indo;
+    cout << "6. Bahasa Inggris\t\t: "; cin >> inggris;
+}
+
+void kasus3P4(){
+    int jam,upah,reguler, total;
+    double over;
+    string nama;
+    upah = 20000;
+
+    cout << "Program Upah Karyawan" << endl;
+    cout << "---------------------" << endl;
+    cout << "PT Indonesia Emas" << endl;
+    cout << "Perusahaan yang berfokus dibidang teknologi dan infrastruktur Indonesia\n" << endl;
+
+    cout << "Informasi Karyawan" << endl;
+    cout << "------------------" << endl;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cout << "Nama Karyawan\t: "; getline(cin, nama);
+    cout << "Jam Kerja\t: "; cin >> jam;
+
+    reguler = upah * jam;
+
+    if(jam >= 30) {
+        over = (jam-30) * upah * (30.0/100.0);
+    } else {
+        over = upah * (20.0/100.0);
+    }
+
+    total = reguler+over;
+
+    cout << "\n--------------------------------------------" << endl;
+    cout << "Nama Karyawan\t: " << nama << endl;
+    cout << "Jam Kerja\t: " << jam << " Jam" << endl;
+    cout << "Upah Kerja selama " << jam << " jam, adalah Rp." << total << endl;
+    cout << "--------------------------------------------\n" << endl;
+}
 
 void sistemP4(){
     int pilihan;
@@ -65,8 +134,8 @@ void sistemP4(){
         do{
             cout << "Program Pertemuan 4 :" << endl;
             cout << "------------------" << endl;
-            cout << "1. Kasus 1 : Penilaian Raport Siswa" << endl;
-            cout << "2. Kasus 2 : Kondisi Matematika Sederhana" << endl;
+            cout << "1. Kasus 1 : Kondisi Matematika Sederhana" << endl;
+            cout << "2. Kasus 2 : Penilaian Raport Siswa" << endl;
             cout << "3. Kasus 3 : Upah Lembur Kayawan" << endl;
             cout << "------------------" << endl;
             cout << "0. Kembali\n " << endl;
@@ -81,8 +150,8 @@ void sistemP4(){
             case 2:
                 kasus2P4();
                 break;
-            /*case 3:
-                kasus3P4();*/
+            case 3:
+                kasus3P4();
             case 0:
                 return;
             default:
