879d01878d1656540603b4517e9c54b15449a32a
Zayd Surya Marthana <zaydsuryamarthana@gmail.com>
2025-10-17 19:38:04 +0700

    upd-p6
    Update Pertemuan 6 Looping atau Perulangan

--- p6.cpp
+++ p6.cpp
@@ -1,118 +1,225 @@
-#include <iostream>
 #include "p6.h"
+#include <iostream>
 #include <limits>
 
 using namespace std;
 
 void kasus1P6(){
-    int a;
-    char pilihan;
-    string kalimat;
-    do {
-        cout << "\nProgram Perulangan atau Looping" << endl;
-        cout << "-------------------------------" << endl;
-        cin.ignore(numeric_limits<streamsize>::max(),'\n');
-        cout << "Silahkan tulis apapun disini\t: "; getline(cin,kalimat);
-        cout << "Mau Looping berapa kali?\t: "; cin >> a;
-        cout << "-------------LOOPING-------------" << endl;
-        for(int i = 1; i<=a; i++){
-            cout << kalimat << " | Loop ke - " << i << endl;
+    auto soal1P6 = [](){
+        int n;
+        cout << "\nKasus 1 Perulangan atau Looping" << endl;
+        cout << "---------------------------------" << endl;
+        cout << "Masukkan Perulangan [Hello World] : "; cin >> n;
+        cout << "-----------------------------------" << endl;
+        for (int i=0; i<n; i++){
+            cout << "Hello World | Loop ke - " << (i+1) << endl;
         }
-        cout << "---------------------------------\n" << endl;
-        cout << "Apakah kamu ingin mengulanginya lagi? [y/n] : "; cin >> pilihan;
-    } while ((pilihan == 'y') || (pilihan == 'Y'));
+        cout << "-----------------------------------" << endl;
+    };
 
-};
+    auto soal2P6 = [](){
+        cout << "\nKasus 2 Deret Angka" << endl;
+        cout << "---------------------" << endl;
+        for (int i=0; i<10; i++){
+            cout << i << " ";
+        }
+        cout << "\n-----------------------" << endl;
+        for (int i=1; i<=10; i++){
+            cout << i << " ";
+        }
+        cout << "\n-----------------------" << endl;
+        for (int i=10; i>=0; i--){
+            cout << i << " ";
+        }
+        cout << "\n-----------------------" << endl;
+        for (int i=9; i>0; i--){
+            cout << i << " ";
+        }
+        cout << "\n-----------------------" << endl;
+    };
+
+    soal1P6();
+    soal2P6();
+}
 
 void kasus2P6(){
+    int a,n,b;
     char pilihan;
-    int a,b,c;
-    do {
-        cout << "\nProgram Perulangan Deeret Matematika" << endl;
-        cout << "---------------------------------------" << endl;
-        cout << "Masukkan nilai a (Nilai Awal)\t: "; cin >> a;
-        cout << "Masukkan nilai b (Nilai Akhir)\t: "; cin >> b;
-        cout << "Masukkan nilai c (Increment)\t: "; cin >> c;
-        cout << '\n';
-        cout << "Deret Matamatika Naik" << endl;
-        cout << "---------------------" << endl;
-        for(int i=a; i<=b; i+=c){
-            cout << i << " ";
-        }
-        cout << '\n' << endl;
-        cout << "Deret Matamatika Menurun" << endl;
-        cout << "------------------------" << endl;
-        for (int i=b; i>=a; i-=c){
-            cout << i << " ";
-        }
+    auto soal1P6 = [&](){
+        do {
+            cout << "\nKasus 2a Modifikasi Deret Angka" << endl;
+            cout << "---------------------------------" << endl;
+            cout << "Input a [Nilai Awal]\t: "; cin >> a;
+            cout << "Input n [Nilai Deret]\t: "; cin >> n;
+            cout << "---------------------------------" << endl;
+            for (int i=a; i<=n; i++){
+                cout << i << " ";
+            }
+            cout << "\n---------------------------------" << endl;
+            cout << "Apakah kamu ingin mengulanginya lagi [y/n] : "; cin >> pilihan;
+        } while((pilihan == 'y') || (pilihan == 'Y'));
+    };
 
-        cout << "\nApakah kamu ingin mengulanginya lagi? [y/n] : "; cin >> pilihan;
-    } while((pilihan == 'y') || (pilihan == 'Y'));
+    auto soal2P6 = [&](){
+        do{
+            cout << "\nKasus 3a Modifikasi Deret Angka Menurun" << endl;
+            cout << "-----------------------------------------" << endl;
+            cout << "Input a [Nilai Awal]\t: "; cin >> a;
+            cout << "Input n [Nilai Deret]\t: "; cin >> n;
+            cout << "-----------------------------------------" << endl;
+            if(a>n){
+                for(int i=a; i>=n; i--){
+                    cout << i << " ";
+                }
+            } else {
+                cout << "\nNilai N Lebih besar dari Nilai Awal tidak bisa menurunkan" << endl;
+            }
+            cout << "\n-----------------------------------------" << endl;
+            cout << "Apakah kamu ingin mengulanginya lagi [y/n] : "; cin >> pilihan;
+        } while((pilihan == 'y') || (pilihan == 'Y'));
+    };
+
+    auto soal3P6 = [&](){
+        do{
+            cout << "\nKasus 4 Modifikasi Program Deret" << endl;
+            cout << "-----------------------------------------" << endl;
+            cout << "Input a [Nilai Awal]\t: "; cin >> a;
+            cout << "Input n [Nilai Deret]\t: "; cin >> n;
+            cout << "Input b [Increment]\t: "; cin >> b;
+            cout << "-----------------------------------------" << endl;
+            for(int i=a; i<=n; i+=b){
+                cout << i << " ";
+            }
+            cout << "\n-----------------------------------------" << endl;
+            cout << "Apakah kamu ingin mengulanginya lagi [y/n] : "; cin >> pilihan;
+        } while((pilihan == 'y') || (pilihan == 'Y'));
+    };
+
+    soal1P6();
+    soal2P6();
+    soal3P6();
+
 }
 
 void kasus3P6(){
-    char pilihan;
-    int maxx=0,minn=10000,a,angka;
-    double avg, sum=0;
-    do{
-        cout << "\nProgram Perulangan Deret Modifikasi" << endl;
-        cout << "--------------------------------------" << endl;
-        cout << "Mau berapa kali Looping? : "; cin >> a;
-        if (a>20){
-            cout << "Terlalu kebanyakan looping " << a << " kali\n" << endl;
-        } else {
-            for (int i = 1; i<=a; i++){
-                cout << "Masukkan nilai ke-" << i << " : "; cin >> angka;
-                if(angka<minn){
-                    minn = angka;
-                }
-                if(angka>maxx){
-                    maxx = angka;
-                }
-                sum = sum + angka;
-            }
-            cout << "--------------------------------------" << endl;
-            cout << "Bilangan Terkecil adalah\t: " << minn << endl;
-            cout << "Bilangan Terbesar adalah\t: " << maxx << endl;
-            cout << "Penjumlahan\t\t\t: " << sum << endl;
-            avg = sum/a;
-            cout << "Rata-rata\t\t\t: " << avg << endl;
-            cout << '\n';
+int a,minn=100000,maxx=0;
+double avg,sum=0;
+int pilihan;
+    auto soal1P6 = [&](){
+        cout << "\nKasus 1 Penjumlahan Deret" << endl;
+        cout << "---------------------------" << endl;
+        for (int i=1; i<=5; i++){
+            cout << i << " ";
+            sum = sum+i;
         }
-        cout << "Apakah kamu ingin mengulanginya lagi? [y/n] : "; cin >> pilihan;
-        cout << '\n';
-    } while((pilihan == 'y')||(pilihan == 'Y'));
+        cout << "\nHasil Penjumlahan : " << sum << endl;
+        cout << "---------------------------\n" << endl;
+
+        sum = 0;
+
+        cout << "Kasus 1a Penjumlahan Deret" << endl;
+        cout << "----------------------------" << endl;
+        for (int i=1; i<=5; i++){
+            cout << "Input bilangan ke-" << i << " : "; cin >> a;
+            sum = sum + a;
+        }
+        cout << "Hasil Penjumlahan : " << sum << endl;
+        cout << "---------------------------" << endl;
+    };
+
+    auto soal2P6 = [&](){
+        cout << "\nKasus 2 Terbesar dan Terkecil" << endl;
+        cout << "-------------------------------" << endl;
+        for (int i=1; i<=10; i++){
+            cout << i << " ";
+            if (i<minn){
+                minn = i;
+            }
+            if (i>maxx){
+                maxx = i;
+            }
+        }
+        cout << "\nAngka Terkecil : " << minn << endl;
+        cout << "Angka Terbesar : " << maxx << endl;
+        cout << "-------------------------------\n" << endl;
+        minn = 100000;
+        a = 0;
+        maxx = 0;
+        sum = 0;
+
+        cout << "Kasus 2a Modifikasi Kasus 1 & 2" << endl;
+        cout << "---------------------------------" << endl;
+        for (int i=1; i<=5; i++){
+            cout << "Input bilangan ke-" << i << " : "; cin >> a;
+            if(a<minn){
+                minn = a;
+            }
+            if (a>maxx){
+                maxx = a;
+            }
+            sum = sum + a;
+        }
+        cout << "---------------------------------" << endl;
+        cout << "\nBilangan Terkecil\t: " << minn << endl;
+        cout << "Bilangan Terbesar\t: " << maxx << endl;
+        cout << "Hasil Penjumlahan\t: " << sum << endl;
+        avg = sum / 5;
+        cout << "Rata - Rata\t\t: " << avg << endl;
+    };
+
+    auto soal3P6 = [](){
+        cout << "\nKasus 3 Modifikasi Deret" << endl;
+        cout << "--------------------------" << endl;
+        for (int i=1; i<=10; i++){
+            if(i==2){
+                cout << (i*i) << " ";
+            } else {
+                cout << i << " ";
+            }
+        }
+    };
+
+    cout << "Kasus dari Modifikasi Input Deret" << endl;
+    cout << "---------------------------------" << endl;
+    cout << "1. Kasus 1 dan Kasus 1a" << endl;
+    cout << "2. Kasus 2 dan Kasus 2a" << endl;
+    cout << "3. Kasus 3 Modifikasi Deret\n" << endl;
+    cout << "Pilih studi kasus : "; cin >> pilihan;
+    switch (pilihan){
+        case 1 :
+            soal1P6();
+            break;
+        case 2 :
+            soal2P6();
+            break;
+        case 3 :
+            soal3P6();
+            break;
+        default :
+            break;
+        }
 }
 
 void sistemP6(){
+    char ulangi;
     int pilihan;
-    cout << "\n----------------------------------" << endl;
-    cout << "Pertemuan 6 : Looping For While Do" << endl;
-    cout << "----------------------------------" << endl;
-        do{
-            cout << "Program Pertemuan 6 :" << endl;
-            cout << "------------------------------------------" << endl;
-            cout << "1. Kasus 1 : Perulangan atau Looping Dasar" << endl;
-            cout << "2. Kasus 2 : Perulangan Deret" << endl;
-            cout << "3. Kasus 3 : Perulangan Deret Modifikasi" << endl;
-            cout << "------------------------------------------" << endl;
-            cout << "0. Kembali\n " << endl;
-            cout << "Pilih salah satu program : "; cin >> pilihan;
-
-            switch (pilihan)
-            {
-            case 1:
-                kasus1P6();
-                break;
-            case 2:
-                kasus2P6();
-                break;
-            case 3:
-                kasus3P6();
-                break;
-            default:
-                break;
-            }
-
-        } while (pilihan != 0);
+    cout << "\nProgram Perulangan atau Looping" << endl;
+    cout << "-------------------------------" << endl;
+    cout << "1. Pengenalan Perulangan atau Looping" << endl;
+    cout << "2. Modifikasi Deret Matematika Looping" << endl;
+    cout << "3. Modifikasi Input Deret Matematika\n" << endl;
+    cout << "Pilih salah satu program : "; cin >> pilihan;
+    switch(pilihan){
+        case 1 :
+            kasus1P6();
+            break;
+        case 2 :
+            kasus2P6();
+            break;
+        case 3 :
+            kasus3P6();
+            break;
+        default :
+            break;
+    }
 }
